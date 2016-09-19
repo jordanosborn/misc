11f03753a7d9d7708c2d1d4494e635c72131bc49
jordanosborn <jordanosborn0@gmail.com>
2016-09-19 05:13:47 +0100

    commit

--- football/poisson_goals.py
+++ football/poisson_goals.py
@@ -15,6 +15,26 @@ random.seed()
 def match_length():
 	return LENGTH+abs(random.gauss(STOPPAGE,STOPPAGE_STDEV))
 
+def select_picks(TEAMS):
+	pairings=[]
+	game_week=TEAMS/2.0
+	tlp=[]
+	for i in range(0,TEAMS*(TEAMS-1)):
+		ls=[x for x in range(0,TEAMS)]
+		if a<=int(TEAMS*(TEAMS-1.0)/2.0)
+			for i in range(0,game_week):
+				while True:
+					rand1=random.randint(0,len(ls))
+
+					rand2=random.randint(0,len(ls))
+					pick=()
+					if doesnt_contain_reverse(pairings,pick):
+						pairings.append(pick)
+						del ls[]
+		else:
+
+
+
 def doesnt_contain_reverse(ls,pick):
 	flag=True
 	for element in ls:
@@ -22,6 +42,22 @@ def doesnt_contain_reverse(ls,pick):
 			flag= False
 			break
 	return flag
+
+def not_played_GW(pairings,pick,TEAMS):
+	a=len(pairings)
+	games=TEAMS/2.0
+	start=int(int(a/games)*TEAMS/2.0)
+	end=int(start+a%int(games))
+	flag=True
+	for p in pairings[start:end]:
+		print(p,pick)
+		input()
+		if(p[0]==pick[0] or p[0]==pick[1] or p[1]==pick[0] or p[1]==pick[1]):
+			flag=False
+			break
+	print(flag)
+	return flag
+
 
 def load_data(name):
 	table=[]
@@ -56,6 +92,7 @@ def load_data(name):
 def populate_fixtures(TEAMS=20):
 	pairings=[]
 	tmp_list=[]
+	used=[]
 	for i in range(0,TEAMS):
 		for j in range(0,TEAMS):
 			if(j==i):
@@ -65,13 +102,14 @@ def populate_fixtures(TEAMS=20):
 	while len(tmp_list)>0:
 		while True:
 			rand_int=random.randint(0,len(tmp_list)-1)
+			
 			pick=tmp_list[rand_int]
-			if len(tmp_list)<=int(TEAMS*(TEAMS-1.0)/2.0):
+			if len(tmp_list)<=int(TEAMS*(TEAMS-1.0)/2.0) and not_played_GW(pairings,pick,TEAMS):
 				pairings.append(pick)
 				del tmp_list[rand_int]
 				break
 			else:
-				if doesnt_contain_reverse(pairings,pick):
+				if doesnt_contain_reverse(pairings,pick) and not_played_GW(pairings,pick,TEAMS):
 					#print(rand_int,len(tmp_list))
 					pairings.append(pick)
 					del tmp_list[rand_int]
@@ -136,22 +174,29 @@ class team:
 				self.properties["Against"]+=self.results[-1].goals("A")
 				if self.results[-1].result()=="H":
 					self.properties["Points"]+=POINTS_W
+					self.properties["Win"]+=1
 				elif self.results[-1].result()=="D":
 					self.properties["Points"]+=POINTS_D
+					self.properties["Draw"]+=1
 				else:
 					self.properties["Points"]+=POINTS_L
+					self.properties["Lose"]+=1
 
 			elif t=="A":
 				self.properties["For"]+=self.results[-1].goals("A")
 				self.properties["Against"]+=self.results[-1].goals("H")
 				if self.results[-1].result()=="A":
 					self.properties["Points"]+=POINTS_W
+					self.properties["Win"]+=1
 				elif self.results[-1].result()=="D":
 					self.properties["Points"]+=POINTS_D
+					self.properties["Draw"]+=1
 				else:
 					self.properties["Points"]+=POINTS_L
+					self.properties["Lose"]+=1
 			else:
 				raise ValueError
+			self.properties["Played"]+=1
 		except ValueError:
 			print("Error ")
 			exit
@@ -168,7 +213,7 @@ class team:
 		self.properties[prop]=val
 
 	def __init__(self,name,HF,HA,AF,AA,TEAMS=20):
-		self.properties={"Name":name,"Form":1.0,"Points":0,"For":0,"Against":0,"Position":0}
+		self.properties={"Name":name,"Form":1.0,"Points":0,"For":0,"Against":0,"Position":0,"Win":0,"Draw":0,"Lose":0,"Played":0}
 		self.probability={"home_F":float(HF)/(LENGTH*(TEAMS-1)),"home_A":float(HA)/(LENGTH*(TEAMS-1)),
 					"away_F":float(AF)/(LENGTH*(TEAMS-1)),"home_A":float(AA)/(LENGTH*(TEAMS-1))}
 		self.results=[]
@@ -179,21 +224,29 @@ def populate_teams(table):
 		tmp_list.append(team(table[i].get("Team"),table[i].get("HomeFor"),table[i].get("HomeAgainst"),table[i].get("AwayFor"),table[i].get("AwayAgainst")))
 	return tmp_list
 #must edit teams as well
-def populate_matches(teams,fixtures):
+#should populate sequentially
+def populate_matches(teams,fixtures,GW=None):
+	if GW==None:
+		start=0
+		end=len(teams)*(len(teams)-1)
+	else:
+		start=int((GW-1)*(len(teams)/2))
+		end=int(start+(len(teams)/2))
 	tmp_list=[]
-	for i,element in enumerate(fixtures):
-		tmp_list.append(match(teams[element[0]],teams[element[1]],i))
-		teams[element[0]].set_match(tmp_list[i],"H")
-		teams[element[1]].set_match(tmp_list[i],"A")
-		teams[element[0]].set_form()
-		teams[element[1]].set_form()
+	for i in range(start,end):
+		tmp_list.append(match(teams[fixtures[i][0]],teams[fixtures[i][1]],i))
+		teams[fixtures[i][0]].set_match(tmp_list[-1],"H")
+		teams[fixtures[i][1]].set_match(tmp_list[-1],"A")
+		teams[fixtures[i][0]].set_form()
+		teams[fixtures[i][1]].set_form()
 
-
-		print(teams[element[0]].get("Name")," vs ",teams[element[1]].get("Name")," Played")
-
+		print(teams[fixtures[i][0]].get("Name")," vs ",teams[fixtures[i][1]].get("Name")," Played")
+	return tmp_list
 
 def print_table(teams):
-	pass
+	#sort by pos
+	for i in teams:
+		print(i.get("Position"),i.get("Name"),i.get("Played"),i.get("Win"),i.get("Draw"),i.get("Lose"),i.get("For"),i.get("Against"),i.get("Points"))
 
 def plot_graphs(teams,matches):
 	pass
@@ -202,6 +255,6 @@ data=load_data("data/premtable1415.csv")
 TEAMS = len(data)
 fixtures=populate_fixtures(TEAMS)
 teams=populate_teams(data)
-matches=populate_matches(teams,fixtures)
-for i in teams:
-	print(i.get("Name"),i.get("Points"))
+matches=populate_matches(teams,fixtures,5)
+print_table(teams)
+
