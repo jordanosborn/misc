1e1e27e7eb8757e83ba875f7d0e2f11b3fb04e96
Jordan Osborn <jordanosborn0@gmail.com>
2018-09-23 12:31:59 +0100

    add

--- rustBook.py
+++ rustBook.py
@@ -1,6 +1,6 @@
 """Create pdf from web book. Jordan Osborn 2018."""
 
-import requests, bs4, re, pdfkit
+import requests, bs4, re, pdfkit, ftfy
 from multiprocessing import Pool
 from sys import argv
 
@@ -19,6 +19,21 @@ def download(url: str) -> str:
     print("Downloaded " + url)
     return text
 
+
+def get_page(url: str) -> str:
+    """Get Page.
+
+    Arguments:
+        url {str} -- url whose content to download
+
+    Returns:
+        str -- return page text
+
+    """
+    text = bs4.BeautifulSoup(download(url), "html.parser").find("div")
+    return ftfy.fix_text(str(text))
+
+
 if __name__ == "__main__":
     root = argv[1]
     contentPage = argv[2]
@@ -27,13 +42,15 @@ if __name__ == "__main__":
     baseURIReg = re.compile(r"(http|https):\/\/(.+)")
     baseURI = baseURIReg.findall(root)[0][1]
     reg = re.compile(r".+?#.*")
-    print("hello")
     urls = map(lambda t: t.get("href"),  bs4.BeautifulSoup(download(root + "/" + contentPage), "html.parser").find("nav").find_all("a"))
     chapters = [contentPage] + list(filter(lambda t: not (t.find("http") == 0 and t.find(baseURI) == -1 or reg.match(t)), urls))
     links = list(map(lambda l: (root + "/" + l) if l.find(baseURI) == -1 else (l), chapters))
 
-    print(links)
     with Pool(10) as p:
-        document = p.map(download, links)
+        document = p.map(get_page, links)
 
     pdfkit.from_string("\n".join(document), output)
+
+#run with
+# rustBook.py https://doc.rust-lang.org/rust-by-example index.html rustExample.pdf
+# rustBook.py https://doc.rust-lang.org/book/2018-edition index.html rustExample.pdf
