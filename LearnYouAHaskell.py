49eaffc75b67772cfaebcfdfa0b99f4a04aba0d7
Jordan Osborn <jordanosborn0@gmail.com>
2018-09-23 18:56:08 +0100

    add

--- LearnYouAHaskell.py
+++ LearnYouAHaskell.py
@@ -1,9 +1,14 @@
 """Create pdf from web book. Jordan Osborn 2018."""
 
 import requests, bs4, re, pdfkit
+from typing import List
 from collections import OrderedDict
 from multiprocessing import Pool
 from sys import argv
+import os
+from shutil import rmtree
+
+TMPOUTPUT = "haskellWeboutput"
 
 def download(url: str) -> str:
     """Download url.
@@ -20,6 +25,21 @@ def download(url: str) -> str:
     print("Downloaded " + url)
     return text
 
+def save(content:tuple) -> None:
+    """Save file.
+
+    Arguments:
+        content {str} -- file content
+
+    Returns:
+        str -- [description]
+
+    """
+    print("Saving file " + content[0])
+    with open(TMPOUTPUT + "/" + content[0]+".html", "w") as f:
+        f.write(content[1])
+    print("Saved file " + content[0])
+
 if __name__ == "__main__":
     root = argv[1]
     contentPage = argv[2]
@@ -31,11 +51,22 @@ if __name__ == "__main__":
     reg = re.compile(r".+?#.*")
     urls = map(lambda t: t.get("href"),  bs4.BeautifulSoup(download(root + "/" + contentPage), "html.parser").find_all("a"))
     chapters = [contentPage] + list(filter(lambda t: not (t.find("http") == 0 and t.find(baseURI) == -1 or reg.match(t)), urls))
-    links = list(map(lambda l: (root + "/" + l) if l.find(baseURI) == -1 else (l), chapters))
-
+    links: List[str] = list(map(lambda l: (root + "/" + l) if l.find(baseURI) == -1 else (l), chapters))
+    order = [str(x) for x in range(0,len(links))]
+    try:
+        rmtree(TMPOUTPUT)
+    except FileNotFoundError:
+        print("Created output directory")
+    os.mkdir(TMPOUTPUT)
     with Pool(10) as p:
         document = p.map(download, links)
+    d = list(zip(order, document))
+    with Pool(10) as p:
+        p.map(save, d)
     print("\n\nDownloaded all files\n\n")
-    pdfkit.from_string("\n".join(document), output)
-    
-    print("PDF created")
+
+    print("Creating " + output)
+    pdfkit.from_file(["haskellWebOutput/" + x + ".html" for x in order], output)
+    print( "Created " + output)
+
+#python3 learnYouAHaskell.py http://learnyouahaskell.com chapters output.pdf
