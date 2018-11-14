5246bda32855dbc5fd00eb79bb5711a0eaa7a668
Jordan Osborn <jordanosborn@users.noreply.github.com>
2018-11-14 13:06:48 +0000

    Update conway.py

--- conway.py
+++ conway.py
@@ -4,20 +4,18 @@ from random import randint
 
 
 def conway(state: Set[Tuple[int, int]], max_x: int = 10, max_y: int = 10) -> Set[Tuple[int, int]]:
-    yield state
     positions = list(itertools.product(range(max_x), range(max_y)))
+    yield state
     while len(state) != 0:
-        # np array with booleans might be quicker
         for x, y in positions:
             alive = False
             if (x, y) in state:
                 state.remove((x,y))
                 alive = True
-            neighbours = functools.reduce(lambda acc, x: acc + (1 if x in state else 0), [
-                (x - 1, y - 1), (x, y - 1), (x + 1, y  - 1),
+            neighbours = functools.reduce(lambda acc, v: acc + (1 if v in state else 0), [
+                (x - 1, y - 1), (x, y - 1), (x + 1, y - 1),
                 (x - 1, y),                 (x + 1, y),
-                (x - 1, y + 1), (x, y + 1), (x + 1, y + 1)
-            ], 0)
+                (x - 1, y + 1), (x, y + 1), (x + 1, y + 1)], 0)
             if alive and neighbours in [2, 3] or not alive and neighbours == 3: state.add((x,y))
         yield state
 
@@ -46,10 +44,6 @@ def main(initial_state: Set[Tuple[int, int]], width=500, height=500, max_x=100, 
 
 
 if __name__ == "__main__":
-    width, height = 1000, 1000
-    max_x, max_y = 500, 500
-    s = set()
-    for x, y in itertools.product(range(max_x), range(max_y)):
-        if randint(0, 1):
-            s.add((x, y))
+    width, height, max_x, max_y = 1000, 1000, 500, 500
+    s = {(x,y) for x, y in itertools.product(range(max_x), range(max_y)) if randint(0, 1)}
     main(s, width, height, max_x, max_y)
