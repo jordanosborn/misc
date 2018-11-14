8fb8509215741311c058dd4677dd88f6ae73cbd8
Jordan Osborn <jordanosborn@users.noreply.github.com>
2018-11-14 11:22:35 +0000

    Update conway.py

--- conway.py
+++ conway.py
@@ -1,77 +1,55 @@
-import pygame
-from pygame.locals import *
+import pygame, functools, itertools
 from typing import Set, Tuple
-from itertools import product
-from functools import reduce
-from time import sleep
+from random import randint
 
 
 def conway(state: Set[Tuple[int, int]], max_x: int = 10, max_y: int = 10) -> Set[Tuple[int, int]]:
     yield state
+    positions = list(itertools.product(range(max_x), range(max_y)))
     while len(state) != 0:
-        #refactor this into array index scheme or nparray
-        for x, y in product(range(max_x), range(max_y)):
+        # np array with booleans might be quicker
+        for x, y in positions:
             alive = False
             if (x, y) in state:
                 state.remove((x,y))
                 alive = True
-            xs, ys = x - 1, y -1
-            neighbours = reduce(lambda acc, x: acc + (1 if x in state else 0), [
-                (xs, ys),
-                (xs, ys + 1),
-                (xs, ys + 2),
-                (xs + 1, ys),
-                (xs + 1, ys + 2),
-                (xs + 2, ys),
-                (xs + 2, ys + 1),
-                (xs + 2, ys + 2)
+            neighbours = functools.reduce(lambda acc, x: acc + (1 if x in state else 0), [
+                (x - 1, y - 1), (x, y - 1), (x + 1, y  - 1),
+                (x - 1, y),                 (x + 1, y),
+                (x - 1, y + 1), (x, y + 1), (x + 1, y + 1)
             ], 0)
-            # rules
-            if alive and neighbours in [2, 3] or not alive and neighbours == 3:
-                state.add((x,y))
+            if alive and neighbours in [2, 3] or not alive and neighbours == 3: state.add((x,y))
         yield state
-    return None
 
 
 def main(initial_state: Set[Tuple[int, int]], width=500, height=500, max_x=100, max_y=100) -> None:
     pygame.init()
-    assert width > 0 and height > 0 and max_x > 0 and max_y > 0
-    if len(list(filter(lambda s: s[0] < 0 or s[0] >= max_x or s[1] < 0 or s[1] >= max_y, initial_state))) != 0:
-        raise ValueError(f"max_x: {max_x-1} max_y: {max_y-1} given: {initial_state}")
-    size_x = width // max_x
-    size_y = height // max_y
-
     screen = pygame.display.set_mode((width, height))
+    size_x, size_y = width // max_x, height // max_y
 
-    def inp():
+    def input_loop():
         while True:
             event = pygame.event.wait()
-            if event.type == pygame.QUIT:
-                pygame.quit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE or event.unicode == 'q':
-                    pygame.quit()
+            if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE: pygame.quit()
 
-    def game():
-        for i, s in enumerate(conway(initial_state, max_x, max_y)):
+    def game_loop():
+        for s in conway(initial_state, max_x, max_y):
             screen.fill((255, 255, 255))
-            for x, y in s:
-                pygame.draw.rect(screen, (0, 0, 0), [x * size_x, y * size_y, size_x, size_y])
+            for x, y in s: pygame.draw.rect(screen, (0, 0, 0), [x * size_x, y * size_y, size_x, size_y])
             pygame.display.flip()
 
-    t1 = pygame.threads.Thread(target=inp, daemon=True)
-    t2 = pygame.threads.Thread(target=game, daemon=True)
-    t1.start()
-    t2.start()
+    t1 = pygame.threads.Thread(target=input_loop, daemon=True)
+    t2 = pygame.threads.Thread(target=game_loop, daemon=True)
+    t1.start(); t2.start()
     t2.join()
     pygame.quit()
 
-from random import randint
+
 if __name__ == "__main__":
     width, height = 1000, 1000
     max_x, max_y = 500, 500
     s = set()
-    for x, y in product(range(max_x), range(max_y)):
+    for x, y in itertools.product(range(max_x), range(max_y)):
         if randint(0, 1):
             s.add((x, y))
     main(s, width, height, max_x, max_y)
